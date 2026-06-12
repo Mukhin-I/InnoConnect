5bfe9a8bd6c29577317958a841823c85b181ea88
Hramov2 <hramovpi@gmail.com>
2026-06-12 15:47:38 +0300

    refactor(env): refactored .env file configureation and location. Know for getting vars use pkg/config package

--- backend/internal/request/app/app.go
+++ backend/internal/request/app/app.go
@@ -1,10 +1,8 @@
 package app
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"gitlab.pg.innopolis.university/innoconnect-team/innoconnect/pkg/config"
 	"gitlab.pg.innopolis.university/innoconnect-team/innoconnect/pkg/logger"
 )
 
@@ -15,8 +13,7 @@ func CreateServer() {
 	setEndpoints(router)
 
 	// Getting .env vars
-	godotenv.Load()
-	requestPort := os.Getenv("REQUEST_SERVICE_PORT")
+	requestPort := config.GetVar("REQUEST_SERVICE_PORT")
 
 	logger.Info("Request service running on :" + requestPort)
 
---
+++ backend/pkg/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetVar(name string) string {
+	godotenv.Load("../.env")
+	return os.Getenv(name)
+}
