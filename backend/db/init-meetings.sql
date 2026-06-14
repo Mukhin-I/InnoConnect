1710f6db5504cfcac681635e62d0b5b31d9d6bb2
CatRespect <alexej.feofanov@mail.ru>
2026-06-14 23:29:15 +0300

    feat(docker) add all structure support[NOT TESTED]

--- backend/db/init-meetings.sql
+++ backend/db/init-meetings.sql
@@ -1,4 +1,4 @@
-CREATE TABLE meetings (
+CREATE TABLE IF NOT EXISTS meetings(
     id BIGSERIAL PRIMARY KEY,
     creator_id BIGINT NOT NULL,
     creator_name TEXT NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE meetings (
     max_people INTEGER
 );
 
-CREATE TABLE meeting_participants (
+CREATE TABLE IF NOT EXISTS meeting_participants (
     meeting_id BIGINT NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
     user_id BIGINT NOT NULL,
     user_name TEXT NOT NULL,
