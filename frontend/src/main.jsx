a9fdfd7bb0c494541ff87b47cc862d84803edc19
dilfd2006 <d.faizullina@innopolis.university>
2026-06-14 21:06:16 +0300

    feat(map): added routing from Map page to the MeetingCreation and back

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Map from './Map.jsx'
+import MeetingCreation from './MeetingCreation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Map />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="/create-meeting" element={<MeetingCreation />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
