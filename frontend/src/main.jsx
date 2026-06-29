d2aae0f95314c93ff7f609da4a27f19a42d61f6e
Mukhin-I <ilyamuhin357@gmail.com>
2026-06-29 16:50:36 +0300

    fix(login page): input layout and image positioning

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,11 @@ import MeetingCreation from './MeetingCreation.jsx'
 import RespondToRequest from './RespondToRequest.jsx'
 import RequestCreation from './RequestCreation.jsx'
 import Requests from './Requests.jsx'
+import Chat from './Chat.jsx'
+import Chats from './Chats.jsx'
+import Profile from './Profile.jsx'
+import Welcome from './Welcome.jsx'
+import Register from './Registration.jsx'
 import Login from './Login.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -18,8 +23,14 @@ createRoot(document.getElementById('root')).render(
         <Route path="/request/:id" element={<RespondToRequest />} />
         <Route path="/create-request" element={<RequestCreation />} />
         <Route path="/requests" element={<Requests />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat/:id" element={<Chat />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
+
