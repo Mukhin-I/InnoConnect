84b0e56fb191f3be03e0b0888f2d54704e91d0b7
Mukhin-I <ilyamuhin357@gmail.com>
2026-06-29 17:59:38 +0300

    fix(reguest navigation): adjust header margin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,12 @@ import MeetingCreation from './MeetingCreation.jsx'
 import RespondToRequest from './RespondToRequest.jsx'
 import RequestCreation from './RequestCreation.jsx'
 import Requests from './Requests.jsx'
+import Chat from './Chat.jsx'
+import Chats from './Chats.jsx'
+import Profile from './Profile.jsx'
+import Welcome from './Welcome.jsx'
+import Register from './Registration.jsx'
+import Login from './Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,7 +23,13 @@ createRoot(document.getElementById('root')).render(
         <Route path="/request/:id" element={<RespondToRequest />} />
         <Route path="/create-request" element={<RequestCreation />} />
         <Route path="/requests" element={<Requests />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat/:id" element={<Chat />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
